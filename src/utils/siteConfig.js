cd458fe504336baf5ba0890253d16c9d6828e773
Daniel Alter <daniel.united@windowslive.com>
2023-02-04 18:02:25 +0200

    SEO stuff

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -7,14 +7,14 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Tips & advice for creating successful online communities`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Discover how to create your own successful social network. Get expert tips and advice for content creators and influencers on building thriving online communities.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    shortTitle: `Blog | Public`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `images/favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
 };
