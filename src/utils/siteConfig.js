bf8f5807eeb9212d1a616ca436c022093a02ea54
Daniel Alter <daniel.united@windowslive.com>
2023-02-04 17:11:28 +0200

    Improved URL, cannonical

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,8 +2,8 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `https://public.community`
-            : `https://public.community`, // Site domain. Do not include a trailing slash!
+            ? process.env.SITE_URL || config.siteUrl || `https://public.community/blog`
+            : `https://public.community/blog`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
